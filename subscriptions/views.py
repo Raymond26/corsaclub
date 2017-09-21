95dff2d1c483f6eb6eb9d9ed47ce1532ead429ce
Raymond Lau <raymond.lau.ca@gmail.com>
2017-09-20 22:53:59 -0700

    account for duplicate subscription

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -6,12 +6,16 @@ from .models import Subscription
 
 def subscription_signup(request):
     if request.method == "POST":
+        email_input = request.POST["email-input"]
+        check_existing = Subscription.objects.filter(email=email_input)
+        template = loader.get_template('subscriptions/signup_success.html')
+        if len(check_existing) > 0:
+            return HttpResponse(template.render(None, request))
         subscription = Subscription(
             email=request.POST["email-input"],
             unsubscribed=False
         )
         subscription.save()
-        template = loader.get_template('subscriptions/signup_success.html')
         return HttpResponse(template.render(None, request))
     template = loader.get_template('subscriptions/signup.html')
     return HttpResponse(template.render(None, request))
